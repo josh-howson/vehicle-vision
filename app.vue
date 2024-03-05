0040d2fc8513243b5773926a26e584f241d5cd8e
Josh Howson <52839079+josh-howson@users.noreply.github.com>
2024-03-05 10:20:16 -0800

    api accepts imageUrl

--- app.vue
+++ app.vue
@@ -1,14 +1,44 @@
 <script setup lang="ts">
+import { fileToBase64 } from '@/utilities/files';
+
   const res = ref();
-  const handleClick = async () => {
-    res.value = $fetch('/api/openai');
+  const fileInput: Ref<HTMLInputElement | null> = ref(null);
+
+  const handleSubmit = async (e: Event) => {
+    const files = fileInput.value?.files;
+    if (!files || files.length === 0) {
+      console.error('No file selected.')
+      return;
+    }
+
+    const file = files[0];
+    try {
+      const base64File = await fileToBase64(file) as string;
+      res.value = await $fetch('/api/openai', {
+      method: 'POST',
+      body: {
+        imageUrl: base64File,
+      },
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    });
+    } catch (error) {
+      console.error("Upload failed:", error);
+    }
   }
 </script>
 
 <template>
   <div>
-    Hello, {{ res }}
+    <form
+      @submit.prevent="handleSubmit"
+    >
+      <label>Upload an image</label>
+      <input ref="fileInput" type="file" accept="image/png, image/jpeg" name="image" />
+      <button type="submit">Describe image</button>
+    </form>
 
-    <button @click="handleClick">Click</button>
+    <div>{{ res }}</div>
   </div>
 </template>
