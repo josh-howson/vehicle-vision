e73464f6eb150cd3334cdaf3a75cda7826186bb3
Josh Howson <52839079+josh-howson@users.noreply.github.com>
2024-03-05 10:31:36 -0800

    tweak prompt, pull openai into utils

---
+++ utilities/openai.ts
@@ -0,0 +1,44 @@
+import axios from 'axios';
+import type { OpenAIVisionRequestBody } from '@/types/openai';
+
+const OPENAI_URL = "https://api.openai.com/v1/chat/completions";
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+
+export const parseOpenAIResponseToObject = (str: string) => {
+  str = str.replace(/^```json\s*|```$/g, '');
+  const obj = JSON.parse(str);
+
+  return obj;
+}
+
+export const getOpenAIResponseWithImage = async (prompt: string, imageUrl: string) => {
+  const headers = {
+    'Authorization': `Bearer ${OPENAI_API_KEY}`,
+    'Content-Type': 'application/json',
+    'Cache-Control': 'no-cache',
+  };
+
+  const payload: OpenAIVisionRequestBody = {
+    model: 'gpt-4-vision-preview',
+    messages: [
+      {
+        role: "user",
+        content: [
+          {
+            type: "text",
+            text: prompt,
+          },
+          {
+            type: 'image_url',
+            image_url: {
+              url: imageUrl,
+            }
+          }
+        ],
+      }
+    ],
+    max_tokens: 300,
+  };
+
+  return await axios.post(OPENAI_URL, payload, { headers })
+}
