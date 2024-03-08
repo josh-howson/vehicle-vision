ef7f97eb39786c28f3f5efe5ac769c4452f1b7e8
Josh Howson <52839079+josh-howson@users.noreply.github.com>
2024-03-07 20:13:58 -0800

    add cool bg

--- components/Layout.vue
+++ components/Layout.vue
@@ -20,13 +20,25 @@
 
 <style scoped>
   .layout {
+    --bg-opacity: .7;
     display: flex;
     flex-flow: column nowrap;
     min-height: 100vh;
     min-height: 100dvh;
     text-align: center;
+    background-image: linear-gradient(rgba(255, 255, 255, calc(1 - var(--bg-opacity))), rgba(255, 255, 255, calc(1 - var(--bg-opacity)))), url('/wave-dots.png');
+    background-size: auto 60rem;
+    background-repeat: no-repeat;
+    background-position: center 60vh;
   }
 
+  @media (min-width: 64rem) {
+    .layout {
+      --bg-opacity: .21;
+      background-size: cover;
+      background-position: center 50vh;
+    }
+  }
   .main {
     flex: 1 1 auto;
     display: flex;
